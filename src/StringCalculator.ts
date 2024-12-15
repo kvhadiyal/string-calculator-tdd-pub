229b8977a3de6728a17382b00475de890eb4806e
Kalpesh Hadiyal <kvhadiyal@gmail.com>
2024-12-15 21:37:08 +0530

    Test 6: Support custom delimiters

--- src/StringCalculator.ts
+++ src/StringCalculator.ts
@@ -2,7 +2,19 @@ export class StringCalculator {
   add(numbers: string) {
     try {
       if (!numbers) return 0;
-      const nums = numbers.replace(/\n/g, ",").split(",").map(Number);
+
+      let delimiter = ",";
+      if (numbers.startsWith("//")) {
+        const parts = numbers.split("\n");
+        delimiter = parts[0].substring(2);
+        numbers = parts[1];
+      }
+
+      const nums = numbers
+        .replace(/\n/g, delimiter)
+        .split(delimiter)
+        .map(Number);
+
       return nums.reduce((sum, num) => sum + num, 0);
     } catch (error) {
       console.log(error)
--- tests/StringCalculator.test.ts
+++ tests/StringCalculator.test.ts
@@ -27,4 +27,8 @@ describe("StringCalculator", () => {
         expect(calculator.add("1\n2,3")).toBe(6);
     });
 
+    test("should support custom delimiters", () => {
+        expect(calculator.add("//;\n1;2")).toBe(3);
+    });
+
 });
